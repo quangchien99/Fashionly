6e7c106eb268fbe21db6b4e3e53dda90547a24dc
Chien Pham <phamquangchien170499@gmail.com>
2023-11-28 10:35:16 +0700

    prevent back behavior from HomeScreen return loading from SpashScreen

--- presentation/src/main/java/chn/phm/presentation/base/navigation/FashionlyNavHost.kt
+++ presentation/src/main/java/chn/phm/presentation/base/navigation/FashionlyNavHost.kt
@@ -1,15 +1,21 @@
 package chn.phm.presentation.base.navigation
 
+import android.app.Activity
+import androidx.activity.compose.BackHandler
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import chn.phm.presentation.screens.home.HomeScreen
 import chn.phm.presentation.screens.onboarding.OnBoardingScreen
 import chn.phm.presentation.screens.splash.SplashScreen
+import kotlin.system.exitProcess
 
 @Composable
 fun FashionlyNavHost(navController: NavHostController) {
+    val context = LocalContext.current
+
     NavHost(navController = navController, startDestination = Screen.SplashScreen.route) {
         composable(Screen.SplashScreen.route) {
             SplashScreen(navController)
@@ -19,6 +25,10 @@ fun FashionlyNavHost(navController: NavHostController) {
         }
         composable(Screen.HomeScreen.route) {
             HomeScreen(navController)
+
+            BackHandler {
+                (context as? Activity)?.moveTaskToBack(true)
+            }
         }
     }
 }
