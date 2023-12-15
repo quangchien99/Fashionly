b4c3abf6f0eb64fca57de60950990ccd84c7bb0c
Chien Pham <phamquangchien170499@gmail.com>
2023-12-15 09:26:44 +0700

    update UI bottom sheet and status bar

--- presentation/src/main/java/chn/phm/presentation/base/components/FashionlyBottomSheet.kt
+++ presentation/src/main/java/chn/phm/presentation/base/components/FashionlyBottomSheet.kt
@@ -6,16 +6,20 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.BottomSheetDefaults
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ModalBottomSheet
+import androidx.compose.material3.ShapeDefaults
 import androidx.compose.material3.Text
 import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import chn.phm.presentation.R
+import chn.phm.presentation.base.theme.BackgroundLight
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -23,6 +27,7 @@ fun SuggestionBottomSheet(onDismiss: () -> Unit) {
     val modalBottomSheetState = rememberModalBottomSheetState()
 
     ModalBottomSheet(
+        containerColor = BackgroundLight,
         onDismissRequest = { onDismiss() },
         sheetState = modalBottomSheetState,
         dragHandle = { BottomSheetDefaults.DragHandle() },
@@ -37,7 +42,11 @@ fun SuggestionBottomSheet(onDismiss: () -> Unit) {
             onClick = { onDismiss() },
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(horizontal = 40.dp, vertical = 20.dp)
+                .padding(start = 40.dp, end = 40.dp, bottom = 60.dp),
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color.Blue
+            ),
+            shape = ShapeDefaults.Small,
         ) {
             Text(stringResource(id = R.string.common_confirm))
         }
--- presentation/src/main/java/chn/phm/presentation/base/theme/Theme.kt
+++ presentation/src/main/java/chn/phm/presentation/base/theme/Theme.kt
@@ -10,10 +10,10 @@ import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
-import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
 
 private val DarkColorScheme = darkColorScheme(
     primary = Purple80,
@@ -56,7 +56,10 @@ fun FashionlyTheme(
     if (!view.isInEditMode) {
         SideEffect {
             val window = (view.context as Activity).window
-            window.statusBarColor = colorScheme.primary.toArgb()
+            val insetsController = WindowCompat.getInsetsController(window, window.decorView)
+            insetsController.hide(
+                WindowInsetsCompat.Type.statusBars()
+            )
             WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
         }
     }
